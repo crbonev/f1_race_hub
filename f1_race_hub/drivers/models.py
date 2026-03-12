b4837c6d95f711bccf80788ebf37386bb46f52eb
crbonev <crbonev@gmail.com>
2026-03-12 17:15:25 +0200

    Updated models: drivers\models.py teams\models.py
    Added models:
    drivers\admin.py
    races\admin.py
    teams\admin.py

--- f1_race_hub/drivers/models.py
+++ f1_race_hub/drivers/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from f1_race_hub.teams.models import Team
+
+
 # Create your models here.
 class Driver(models.Model):
     first_name = models.CharField(max_length=50)
@@ -7,3 +10,14 @@ class Driver(models.Model):
     nationality = models.CharField(max_length=50)
     driver_number = models.PositiveIntegerField(unique=True)
     championships = models.PositiveIntegerField(default=0)
+
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='drivers',
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
