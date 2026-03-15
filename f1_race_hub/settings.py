ef153dccb13d04401df6a52e4713b6be0a77a1e3
crbonev <crbonev@gmail.com>
2026-03-15 23:59:03 +0200

    Added custom 404 page

--- f1_race_hub/settings.py
+++ f1_race_hub/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-todx!*j+hmg-d!!i_o#ft_)6=^1g4l-r&t=8)-^=$v20zpu+qs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 MY_APPS = [
     'f1_race_hub.drivers',
