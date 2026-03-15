327e9295acda199149a00ecc286028da67fa9fbb
crbonev <crbonev@gmail.com>
2026-03-16 01:43:52 +0200

    Added races to index.html Added races to views.py

--- f1_race_hub/views.py
+++ f1_race_hub/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from f1_race_hub.drivers.models import Driver
+from f1_race_hub.races.models import Race
 from f1_race_hub.teams.models import Team
 
 
 def index(request):
     drivers_count = Driver.objects.count()
     teams_count = Team.objects.count()
+    races_count = Race.objects.count()
 
     context = {
         "drivers_count": drivers_count,
         "teams_count": teams_count,
+        "races_count": races_count,
     }
 
     return render(request, "index.html", context)
