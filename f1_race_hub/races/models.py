fbc42cd43b1abb13379d47e941dd8196acca099e
crbonev <crbonev@gmail.com>
2026-03-16 09:55:03 +0200

    New races\models.py model

---
+++ f1_race_hub/races/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from f1_race_hub.drivers.models import Driver
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=100)
+    circuit = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    race_date = models.DateField()
+    season = models.PositiveIntegerField()
+
+    drivers = models.ManyToManyField(
+        Driver,
+        through="RaceResult",
+        related_name="races"
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.season}"
+
+
+class RaceResult(models.Model):
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE
+    )
+
+    driver = models.ForeignKey(
+        Driver,
+        on_delete=models.CASCADE
+    )
+
+    position = models.PositiveIntegerField()
+    points = models.PositiveIntegerField()
+    fastest_lap = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.race} | {self.driver} | P{self.position}"
+
+    class Meta:
+        ordering = ["position"]
+        unique_together = ["race", "driver"]
